406e5cd0b876c0f9a73a1bf5bab605ff3130f537
solpierozzi <solpierozzi@hotmail.com>
2021-02-12 15:23:41 -0300

    Cambiado contenido tr

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
           <table class="table">
             <thead>
               <tr>
-                <th scope="col">#id</th>
+                <th scope="col">ID</th>
                 <th scope="col">Nombre</th>
                 <th scope="col">Descripción</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Acciones</th>
               </tr>
             </thead>
             <tbody>
